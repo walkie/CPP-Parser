f2a7f5f7875299acde75bc28f01b15e24697a6fd
benpflaum <benpflaum@gmail.com>
2009-06-23 20:06:33 -0700

    add XmlRootElement attribute
    Ignore-this: 7f0badb652c9b5da3a1f135b14e5523a
    
    darcs-hash:20090624030633-4eb44-2839eab74003cc185f7c91d90e8375e7e4c45ad5.gz

--- editor/src/editor/io/Document.java
+++ editor/src/editor/io/Document.java
@@ -15,6 +15,7 @@ import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlElementRef;
+import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlType;
 
 
@@ -38,6 +39,7 @@ import javax.xml.bind.annotation.XmlType;
  * 
  * 
  */
+@XmlRootElement
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlType(name = "Document", propOrder = {
     "dimensions",
