413eab0b5198d9c53f90ba682a7c22edce9a3633
walkiner <walkiner@eecs.oregonstate.edu>
2009-08-28 13:54:27 -0700

    CPP/Cond.hs: Refactoring to work with C-style expressions.
    Ignore-this: 62b4aca502ff55dc8f90092075a9942b
    
    darcs-hash:20090828205427-76698-16b0cd01abe98b3a87f60c8aefdfbe7ff490583a.gz

--- haskell/CPP/Cond.hs
+++ haskell/CPP/Cond.hs
@@ -1,32 +1,55 @@
 
+module CPP.Cond where
+
+import Control.Monad (liftM,liftM2)
+import Data.Maybe
+
+import CPP.Lang (CExpr(..),Macro,eval)
+import qualified CPP.Lang as C (UnOp(..),BinOp(..))
+
 type Name = String
 
-data Expr = Var Name 
-          | Not Expr
-          | And Expr Expr
-          | Or  Expr Expr
-          deriving (Eq, Show)
+data BExpr = Con Bool
+           | Var Name 
+           | Not BExpr
+           | And BExpr BExpr
+           | Or  BExpr BExpr
+           deriving (Eq, Show)
 
-data Cond a = CN
-            | CD  a
-            | IT  Expr (Cond a)
-            | ITE Expr (Cond a) (Cond a)
+data Cond a = CD  a
+            | IT  BExpr [Cond a]
+            | ITE BExpr [Cond a] [Cond a]
             deriving (Eq, Show)
 
-data Simple a = SN
-              | SD a
-              | SC Name (Simple a) (Simple a)
+mcon = Just . Con
+mvar = Just . Var
+mnot = liftM  Not
+mand = liftM2 And
+mor  = liftM2 Or
+
+boolize :: CExpr -> Maybe BExpr
+boolize (IntConst  i) = mcon (i /= 0)
+boolize (CharConst c) = mcon (fromEnum c /= 0)
+boolize (Defined   v) = mvar v
+boolize (TerIf c t e) = boolize c `mand` boolize t `mor` boolize e
+boolize (UnOp  C.Not e)   = mnot (boolize e)
+boolize (BinOp C.And l r) = boolize l `mand` boolize r
+boolize (BinOp C.Or  l r) = boolize l `mor`  boolize r
+boolize _ = Nothing
+
+-- Translating into "simple" conditional expressions.
+
+data Simple a = SD a
+              | SC Name [Simple a] [Simple a]
               deriving (Eq, Show)
 
-simplify :: Cond a -> Simple a
-
-simplify CN       = SN
-simplify (CD d)   = SD d
-simplify (IT c t) = simplify (ITE c t CN)
-
-simplify (ITE (Var v) t e) = SC v (simplify t) (simplify e)
-simplify (ITE (Not c) t e) = simplify (ITE c e t)
-
-simplify (ITE (And c d) t e) = simplify (ITE c (ITE d t e) e)
-simplify (ITE (Or  c d) t e) = simplify (ITE c t (ITE d t e)) -- data duplication!
-
+simplify :: [Cond a] -> [Simple a]
+simplify = concatMap simp
+  where simp (CD d) = [SD d]
+        simp (IT c t) = simp (ITE c t [])
+        simp (ITE (Con True)  t e) = simplify t
+        simp (ITE (Con False) t e) = simplify e
+        simp (ITE (Var v) t e) = [SC v (simplify t) (simplify e)]
+        simp (ITE (Not c) t e) = simp (ITE c e t)
+        simp (ITE (And c d) t e) = simp (ITE c [ITE d t e] e)
+        simp (ITE (Or  c d) t e) = simp (ITE c t [ITE d t e]) -- data duplication!
