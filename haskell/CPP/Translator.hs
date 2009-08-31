720985c17e59101d63e0ebe8c955938f3fcf564e
walkiner <walkiner@eecs.oregonstate.edu>
2009-08-31 02:21:53 -0700

    CPP/Translator.hs: Added second and third phases to CPP to choice-calc conversion.
    Ignore-this: b7d3701e616cf3fafbcbac238439e107
    
    darcs-hash:20090831092153-76698-3eb738a205ca6bee3f0340429b6d361809dce62a.gz

--- haskell/CPP/Translator.hs
+++ haskell/CPP/Translator.hs
@@ -1,16 +1,16 @@
 
 module CPP.Translator where
 
-import Control.Monad (liftM, liftM2, replicateM)
-import Text.ParserCombinators.Parsec hiding (Line)
+import Control.Monad.State
+import Data.Maybe (catMaybes)
+import Text.ParserCombinators.Parsec hiding (Line, State)
 
 import CPP.Cond
 
 import CPP.Lang hiding (Name, UnOp(..), BinOp(..))
-import qualified CPP.Lang as C
 
---import Choice hiding (parse, text)
---import PointFree
+import Choice hiding (Name, Var, parse, text)
+import qualified Choice
 
 --
 -- Phase 1: Extract conditional structure from parsed CPP.
@@ -32,8 +32,9 @@ extract l fs = either (error . show) id $
 keepText :: LinesTo Text
 keepText _ = Text
 
-discardText :: LinesTo Int
-discardText = const
+discardText :: LinesTo (Maybe Int)
+discardText _ [] = Nothing
+discardText i _  = Just i
 
 -- The primitive line parser.  Match a line for which the predicate is true.
 line :: (Line -> Bool) -> Extract a Line
@@ -59,8 +60,87 @@ cond = do ifL   <- condLine isIf
           thenB <- block
           elifs <- many (liftM2 (,) (condLine isElif) block)
           melse <- option [] (line isElse >> block)
+          line isEndif
           return (build ifL thenB elifs melse)
   where condLine f = liftM condition (line f)
         build c t [] [] = IT  c t
         build c t [] e  = ITE c t e
         build c t ((eic,eit):eis) e = ITE c t [build eic eit eis e]
+
+--
+-- Phase 2: Convert C-expressions into boolean expressions,
+--          removing untranslateable branches.
+--
+
+type Convert = State [CExpr]
+
+convert :: [Cond CExpr a] -> ([Cond BExpr a], [CExpr])
+convert cs = runState (convConds cs) []
+
+convConds :: [Cond CExpr a] -> Convert [Cond BExpr a]
+convConds cs = liftM catMaybes (mapM convCond cs)
+
+convCond :: Cond CExpr a -> Convert (Maybe (Cond BExpr a))
+convCond (CD a)      = (return . Just . CD) a
+convCond (IT c t)    = do t' <- convConds t
+                          fmap2 (\b -> IT b t') (convExpr c)
+convCond (ITE c t e) = do t' <- convConds t
+                          e' <- convConds e
+                          fmap2 (\b -> ITE b t' e') (convExpr c)
+
+convExpr :: CExpr -> Convert (Maybe BExpr)
+convExpr c = maybe (modify (c:) >> return Nothing) (return . Just) (boolize c)
+
+fmap2 = fmap . fmap
+
+--
+-- Phase 3: Translate conditional expressions into the choice syntax,
+--          simplifying where possible.
+--
+
+type Translate = State Int
+
+class    Stored a         where empty :: a
+instance Stored Text      where empty = Text []
+instance Stored (Maybe a) where empty = Nothing
+
+translate :: Stored a => [Cond BExpr a] -> Expr Bool a
+translate cs = foldr ($) body dims
+  where dims = map dim (condVars cs)
+        body = evalState (tconds cs) 0
+
+newVar :: Translate Name
+newVar = do i <- get
+            put (i+1)
+            return ("v" ++ show i)
+
+exprList :: Stored a => [Expr Bool a] -> Expr Bool a
+exprList [e] = e
+exprList es  = empty :< es
+
+tconds :: Stored a => [Cond BExpr a] -> Translate (Expr Bool a)
+tconds cs = liftM exprList (mapM tcond cs)
+
+dim :: Name -> Expr Bool a -> Expr Bool a
+dim n = Dim (n := [True,False])
+
+tcond :: Stored a => Cond BExpr a -> Translate (Expr Bool a)
+tcond (CD d) = return (leaf d)
+tcond (CRef v) = return (Choice.Var v)
+tcond (IT c t) = tcond (ITE c t [])
+tcond (ITE (Con True)  t e) = tconds t
+tcond (ITE (Con False) t e) = tconds e
+tcond (ITE (Var v) t e) = do t' <- tconds t
+                             e' <- tconds e
+                             return (v :? [t',e'])
+tcond (ITE (Not c) t e) = tcond (ITE c e t)
+tcond (ITE (And c d) t e) = do v <- newVar
+                               b <- liftM (v :=) (tconds e)
+                               let e' = [CRef v]
+                               let t' = [ITE d t e']
+                               liftM (Let b) (tcond (ITE c t' e'))
+tcond (ITE (Or  c d) t e) = do v <- newVar
+                               b <- liftM (v :=) (tconds t)
+                               let t' = [CRef v]
+                               let e' = [ITE d t' e]
+                               liftM (Let b) (tcond (ITE c t' e'))
