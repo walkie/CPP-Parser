aaf7e8890a7f085a482815c21ff67ce15f330b07
walkiner <walkiner@eecs.oregonstate.edu>
2009-08-31 02:22:24 -0700

    CPP/Main.hs: Updated main function, added lots of esoteric output.
    Ignore-this: 911db0beeb1e36fd110b808ff99848a6
    
    darcs-hash:20090831092224-76698-7647f4319f05fca51fa82865bda043aea34cfd7f.gz

--- haskell/CPP/Main.hs
+++ haskell/CPP/Main.hs
@@ -1,7 +1,10 @@
-
+{-# LANGUAGE FlexibleInstances #-}
 module CPP.Main where
 
+import Data.List (intersect,nub)
+
 import System.Environment
+import System.IO.Unsafe
 
 import Choice
 import CPP.Lang
@@ -9,13 +12,31 @@ import CPP.Parser
 import CPP.Translator
 
 main = do ps <- getArgs
-          fs <- mapM parseFile ps
-          let cs = map cppToChoice fs
-          mapM_ print cs
+          fs <- mapM (unsafeInterleaveIO . parseFile) ps
+          let cs = extract discardText fs
+          --let cs = extract keepText fs
+          let (cs',bad) = convert cs
+          let e = translate cs'
+          let ds = dom (dims e)
+          let badms = nub (concatMap macros bad)
+          let overlap = ds `intersect` badms
+          putStrLn $ "** Could not convert " ++ show (length bad) ++ " conditions:"
+          print bad
+          putStrLn $ "** Containing " ++ show (length badms) ++ " macros:"
+          putStr (unlines badms)
+          putStrLn $ "** Which overlaps with " ++ show (length overlap) ++ " dimensions in e:"
+          putStr (unlines overlap)
+          putStrLn $ "** Number of dimensions in e: " ++ show (length ds)
+          putStrLn $ "** Total number of macros: " ++ show (length badms + length ds - length overlap)
+          putStrLn "--"
+          print e
 
-instance ShowNesting Expression
+instance ShowNesting Bool
+instance ShowNesting (Maybe Int) where
+  showS (Just i) = show i
+  showS Nothing  = "X"
 instance ShowNesting Text where
-  showS _ = ""
+  showS = show
   opAng _ = ""
   comma _ = ""
   clAng _ = ""
